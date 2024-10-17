a06f08d6555f589cf893eef39eb8a554a9220f9d
Aditkiie <aditkurn7@gmail.com>
2024-10-17 11:08:53 +0700

    part8-9

--- application/controllers/Dosen.php
+++ application/controllers/Dosen.php
@@ -1,27 +1,29 @@
 <?php
-class Dosen extends CI_Controller {
+class Dosen extends CI_Controller
+{
 
     public function __construct()
     {
         parent::__construct();
         $this->load->model('Dosen_model');
-       
- 
     }
 
     public function index()
     {
+        if (empty($this->session->userdata('email'))) {
+            redirect('login');
+        }
         $data['judul'] = 'Halaman Dosen';
         // akhir memasukan data berdasarkan keyword
         $data['dosen'] = $this->Dosen_model->getAllDosen();
-        if ( $this->input->post('keyword') ) {
+        if ($this->input->post('keyword')) {
             $data['dosen'] = $this->Dosen_model->cariDataDosen();
         }
         // akhir memasukan data berdasarkan keword
-        
+
         $this->load->view('templates/header', $data);
         // untuk memanggil data pada folder view
-        $this->load->view('dosen/index',$data);
+        $this->load->view('dosen/index', $data);
         $this->load->view('templates/footer');
     }
     public function tambah()
@@ -34,7 +36,7 @@ class Dosen extends CI_Controller {
             $this->load->view('templates/header', $data);
             $this->load->view('dosen/tambah');
             $this->load->view('templates/footer');
-        }else {
+        } else {
             $this->Dosen_model->tambahDataDosen();
             $this->session->set_flashdata('flash', 'ditambahkan');
             redirect('dosen');
@@ -51,9 +53,9 @@ class Dosen extends CI_Controller {
     public function detail($id)
     {
 
-         $data['judul']= 'Detail Data Dosen';
-         $data['dosen'] =$this->Dosen_model->getDosenById($id);
-        $this->load->view('templates/header'  , $data);
+        $data['judul'] = 'Detail Data Dosen';
+        $data['dosen'] = $this->Dosen_model->getDosenById($id);
+        $this->load->view('templates/header', $data);
         $this->load->view('dosen/detail', $data);
         $this->load->view('templates/footer');
     }
@@ -68,7 +70,7 @@ class Dosen extends CI_Controller {
         $this->form_validation->set_rules('namadosen', 'namadosen', 'required');
         if ($this->form_validation->run() == FALSE) {
             $this->load->view('templates/header', $data);
-            $this->load->view('dosen/ubah',$data);
+            $this->load->view('dosen/ubah', $data);
             $this->load->view('templates/footer');
         } else {
             $this->Dosen_model->ubahDataDosen();
